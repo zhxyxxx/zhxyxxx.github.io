b59c83efefd93f89b69095bad21a1790359fe096
zhxyxxx <41413243+zhxyxxx@users.noreply.github.com>
2020-09-14 03:02:01 +0900

    v1.2 (add languages)

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ function scrollFunction() {
     pagetop.style.display = "none";
   }
 }
+
+
+//switch Language
+var ja = document.querySelectorAll(".Japanese");
+var en = document.querySelectorAll(".English");
+var ch = document.querySelectorAll(".Chinese");
+var j_btn = document.querySelector("#Japanese");
+var e_btn = document.querySelector("#English");
+var c_btn = document.querySelector("#Chinese");
+
+j_btn.onclick = () => {
+  ch.forEach(function(value, index, array) {
+    value.style.display = "none";
+  });
+  en.forEach(function(value, index, array) {
+    value.style.display = "none";
+  });
+  ja.forEach(function(value, index, array) {
+    value.style.display = "block";
+  });
+};
+
+e_btn.onclick = () => {
+  ch.forEach(function(value, index, array) {
+    value.style.display = "none";
+  });
+  ja.forEach(function(value, index, array) {
+    value.style.display = "none";
+  });
+  en.forEach(function(value, index, array) {
+    value.style.display = "block";
+  });
+};
+
+c_btn.onclick = () => {
+  ja.forEach(function(value, index, array) {
+    value.style.display = "none";
+  });
+  en.forEach(function(value, index, array) {
+    value.style.display = "none";
+  });
+  ch.forEach(function(value, index, array) {
+    value.style.display = "block";
+  });
+};
